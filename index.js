adae211e6ae118484fdcda7d54ec06bd545a3ede
Lyssie <lyssiewright.15@gmail.com>
2022-01-31 20:59:48 -0600

    commit tree 5e1e619f070492c55bd52ad645aadf77c08e1164

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    //console.log(`This Saturday, I want to ${activity}!`);
+    return ("This Saturday, I want to " + activity + "!");
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return ("This Monday, I will " + activity + ".");
+}
+
+mondayWork('work from home');
+
+function wrapAdjective(para1 = "*"){
+    //const encouragingPromptFunction = function(para2 = "special"){
+     return function (para2 = "special"){
+        return `You are ${para1}${para2}${para1}!`;
+    };
+    return encouragingPromptFunction;
+}
+
+wrapAdjective("||")("a dedicated programmer");
